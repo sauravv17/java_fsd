aed017d977edcc37987ad25fdb1942b98f9a6896
sauravv17 <sauravgupta.nda@gmail.com>
2022-03-28 12:33:43 +0530

    this fix is for ticket 123

---
+++ com/teksystems/demo1.java
@@ -0,0 +1,28 @@
+package com.teksystems;
+
+public class demo1 {
+	public static void main(String[] args) {
+		int x=4;
+		System.out.println(x);
+		
+		// implicit typecasting
+		int num1=6;
+		int num2=55;
+		System.out.println(num1 + num2);
+		System.out.println("total sum is " + num1 + num2);
+		
+		//data types
+		
+		//Bytes
+		//bytes - 8bits -- value stored -- (-128 to 127) total 2^8 =256
+		
+		byte xbyte=10;
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		//Short 
+		
+
+	}
+
+}
