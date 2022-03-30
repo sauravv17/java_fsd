8b66b6aa546ad7ab75f778c3b11ea485d91f234b
sauravv17 <sauravgupta.nda@gmail.com>
2022-03-30 23:00:43 +0530

    encapsulation

--- encapsulation/Printer.java
+++ encapsulation/Printer.java
@@ -1,38 +1,27 @@
 package encapsulation;
 
 public class Printer {
-	private int tonerLevel;
 	private int pagesPrinted;
 	private boolean duplex;
 	
-	public Printer(int tonerLevel,boolean duplex) {
-		if (tonerLevel >=1 && tonerLevel <=100) {
-			this.tonerLevel = tonerLevel;
-		}else {
-			this.tonerLevel = -1;
-		}
-		
+	public Printer(boolean duplex) {
 		this.pagesPrinted = 0;
 		this.duplex = duplex;
 	}
-	public int addToner(int amount) {
-		if (amount > 0 && amount <=100) {
-			if (this.tonerLevel +amount >100) {
-				return -1;
-			this.tonerLevel+=amount;
-			return this.tonerLevel;
-			}
-		}else {
-			return -1;
-		}
-	}
 	
 	public int printPages(int pages) {
-		System.out.println("printing"+pages +"pages");
+		System.out.println("printing "+pages +" pages");
 		int pagesToPrint =pages;
 		if (this.duplex) {
+			pagesToPrint=(pages/2) + (pages%2);
+			System.out.println("printing in duplex");
 			
 		}
+		this.pagesPrinted +=pagesToPrint;
+		return pagesToPrint;
+	}
+	public int getPagesPrinted() {
+		return this.pagesPrinted;
 	}
 	
 	
