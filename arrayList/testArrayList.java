d098f5a8e8fba7a2fe365aef91cccc23ba774571
sauravv17 <sauravgupta.nda@gmail.com>
2022-03-31 15:52:04 +0530

    3rd day

---
+++ arrayList/testArrayList.java
@@ -0,0 +1,15 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class testArrayList {
+
+	public static void main(String[] args) {
+		ArrayList<> strList=new ArrayList<>();
+		strList.add("Hello");
+		strList.add(1);
+		
+		
+		System.out.println(strList);
+	}
+}
--- com/teksystems/userInput_tut.java
+++ com/teksystems/userInput_tut.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class userInput_tut {
 	public static void main(String[] args) {
-		Scanner scanner = new Ssscanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("name");
 		String name=scanner.nextLine();
 		System.out.println("name entered  " + name);
